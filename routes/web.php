35ddd0d45af3879058b50d360df1d60bc37036ef
DmitryAlex2012 <tpmfd27@gmail.com>
2021-06-23 16:03:07 +0300

    Unused code deleted.

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-
 Route::get('/', 'BookController@index');
 Route::get('book', 'BookController@index')->name('filter');
 
